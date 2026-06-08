1a79d9d64a2976705e0844495b5a400c28732019
airPC <air005831@gmail.com>
2026-06-08 23:44:48 +0800

    文件更新與測試腳本

---
+++ frontend/test_api.js
@@ -0,0 +1,12 @@
+﻿import axiosClient from './src/api/axiosClient.js';
+import venuesApi from './src/api/venues.js';
+
+async function test() {
+    try {
+        const data = await venuesApi.getVenues();
+        console.log(JSON.stringify(data, null, 2));
+    } catch(e) {
+        console.error(e);
+    }
+}
+test();
